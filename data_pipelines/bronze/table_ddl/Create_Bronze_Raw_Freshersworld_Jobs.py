c2afb5c52e140d5e96ac91aedf5944b1ad15d384
SahithiGudivada <sahithigudivada27@gmail.com>
2026-06-27 14:08:13 +0000

    join the fresherworld site into the bronze tables

---
+++ data_pipelines/bronze/table_ddl/Create_Bronze_Raw_Freshersworld_Jobs.py
@@ -0,0 +1,24 @@
+# Databricks notebook source
+# /// script
+# [tool.databricks.environment]
+# environment_version = "5"
+# ///
+# MAGIC %md
+# MAGIC %md
+# MAGIC ## Create_Bronze_Jobs_Raw
+# MAGIC
+# MAGIC | Date         | Modified By      | Change Log             |
+# MAGIC |--------------|------------------|------------------------|
+# MAGIC | apr 3rd 2026| Sahithi Gudivada  | Creating the Table : raw_freshersworld_jobs |
+
+# COMMAND ----------
+
+spark.sql("""
+        CREATE TABLE IF NOT EXISTS jobsintel.bronze.raw_freshersworld_jobs (
+            MESSAGE_ID BIGINT GENERATED ALWAYS AS IDENTITY COMMENT "The identity of the message entering the table sourcing from freshersworld",
+            PAYLOAD STRING  COMMENT "The Json format of the data called from the website",
+            BD_CREATE_DT_TM TIMESTAMP COMMENT "timestamp loaded",
+            BD_UPDATE_DT_TM TIMESTAMP COMMENT "timestamp updated"
+        )
+        USING DELTA
+ """)
