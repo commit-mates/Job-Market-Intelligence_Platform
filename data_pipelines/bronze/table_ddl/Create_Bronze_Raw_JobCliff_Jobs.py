def0287da8a0cd260b7661c1b62d22c5d56a9c4e
SahithiGudivada <sahithigudivada27@gmail.com>
2026-08-06 15:51:08 +0000

    pushing load_jobcliff_feed

---
+++ data_pipelines/bronze/table_ddl/Create_Bronze_Raw_JobCliff_Jobs.py
@@ -0,0 +1,14 @@
+# Databricks notebook source
+# /// script
+# [tool.databricks.environment]
+# environment_version = "5"
+# ///
+spark.sql("""
+    CREATE TABLE IF NOT EXISTS jobsintel.bronze.raw_JobCliff_jobs (
+        MESSAGE_ID BIGINT GENERATED ALWAYS AS IDENTITY COMMENT "The identity of the message entering the table sourcing from JobCliff",
+        PAYLOAD STRING  COMMENT "The Json format of the data called from the website",
+        BD_CREATE_DT_TM TIMESTAMP COMMENT "timestamp loaded",
+        BD_UPDATE_DT_TM TIMESTAMP COMMENT "timestamp updated"
+    )
+    USING DELTA
+""")
