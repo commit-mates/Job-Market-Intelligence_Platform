d7a5e57a86c908beb539ce0ab5442f20d9b43f84
SahithiGudivada <sahithigudivada27@gmail.com>
2026-08-14 15:20:00 +0000

    pushing joinsaarthi feed

---
+++ data_pipelines/bronze/table_ddl/Create_Bronze_Raw_Joinsaarthi_Jobs.py
@@ -0,0 +1,23 @@
+# Databricks notebook source
+# /// script
+# [tool.databricks.environment]
+# environment_version = "5"
+# ///
+# MAGIC %md
+# MAGIC ## Create_Bronze_Raw_Joinsaarthi_Jobs
+# MAGIC
+# MAGIC | Date         | Modified By      | Change Log             |
+# MAGIC |--------------|------------------|------------------------|
+# MAGIC | Aug 14th 2026| Sahithi Gudivada  | Creating the Table : raw_Joinsaarthi_jobs |
+
+# COMMAND ----------
+
+spark.sql("""
+    CREATE TABLE IF NOT EXISTS jobsintel.bronze.raw_joinsaarthi_jobs (
+        MESSAGE_ID BIGINT GENERATED ALWAYS AS IDENTITY COMMENT "The identity of the message entering the table sourcing from Joinsaarthi",
+        PAYLOAD STRING  COMMENT "The Json format of the data called from the website",
+        BD_CREATE_DT_TM TIMESTAMP COMMENT "timestamp loaded",
+        BD_UPDATE_DT_TM TIMESTAMP COMMENT "timestamp updated"
+    )
+    USING DELTA
+""")
